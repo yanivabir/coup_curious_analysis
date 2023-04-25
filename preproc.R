279e5b0209439e2fc3bf9aa223954ffd0ed78bfb
yanivabir <yanivabir@gmail.com>
2023-04-25 09:21:49 +0300

    recode questionnaires

--- preproc.R
+++ preproc.R
@@ -253,77 +253,33 @@ n_quest_na <- data.table(PID = quest$PID, n_miss_quest = rowSums(quest[, ns[grep
                                                                        with = F][, lapply(.SD, is.na)]))
 quality <- merge(quality, n_quest_na, by = "PID", all.x = T)
 
-# Reverse code items ---
+# Reverse code items DOUBLE CHECK ---
 # Anxiety - higher is negative affect
-quest[, anxiety_0 := 3 - (anxiety_0 )] # Calm
-quest[, anxiety_1 := 3 - (anxiety_1)] # Secure
-quest[, anxiety_4 := 3 - (anxiety_4)] # At ease
-quest[, anxiety_7 := 3 - (anxiety_7)] # Satisfied
-quest[, anxiety_9 := 3 - (anxiety_9)] # Comfortable
-quest[, anxiety_10 := 3 - (anxiety_10)] # Self confident
-quest[, anxiety_14 := 3 - (anxiety_14)] # Relaxed
-quest[, anxiety_15 := 3 - (anxiety_15)] # Content
-quest[, anxiety_18 := 3 - (anxiety_18)] # Steady
-quest[, anxiety_19 := 3 - (anxiety_19)] # Pleasant
+quest[, stai_0 := 3 - (stai_0 )] # Calm
+quest[, stai_1 := 3 - (stai_1)] # Secure
+quest[, stai_4 := 3 - (stai_4)] # At ease
+quest[, stai_7 := 3 - (stai_7)] # Satisfied
+quest[, stai_9 := 3 - (stai_9)] # Comfortable
+quest[, stai_10 := 3 - (stai_10)] # Self confident
+quest[, stai_14 := 3 - (stai_14)] # Relaxed
+quest[, stai_15 := 3 - (stai_15)] # Content
+quest[, stai_18 := 3 - (stai_18)] # Steady
+quest[, stai_19 := 3 - (stai_19)] # Pleasant
 
-# Apathy - higher is apatehtic
-quest[, apathy_0 := 3 - (apathy_0)] # interested in learning new things
-quest[, apathy_1 := 3 - (apathy_1)] # anything interest you
-quest[, apathy_3 := 3 - (apathy_3)] # put much effort into things
-quest[, apathy_4 := 3 - (apathy_4)] # always looking for something to do
-quest[, apathy_5 := 3 - (apathy_5)] # plans and goals for the future
-quest[, apathy_6 := 3 - (apathy_6)] # have motivation
-quest[, apathy_7 := 3 - (apathy_7)] # have the energy for daily activities
+# Apathy - higher is motivated
+quest[, apathy_2 := 3 - (apathy_2)] # worried
+quest[, apathy_8 := 3 - (apathy_8)] # tell you what to do
+quest[, apathy_9 := 3 - (apathy_9)] # indifferent
+quest[, apathy_10 := 3 - (apathy_10)] # unconcerned
+quest[, apathy_11 := 3 - (apathy_11)] # push
+quest[, apathy_12 := 3 - (apathy_12)] # neither happy nor sad
+quest[, apathy_13 := 3 - (apathy_13)] # apathetic
 
-# Impulsiveness - higher is impulsive
-quest[, impulsive_0 := 3 - (impulsive_0)] # plan tasks carefully
-quest[, impulsive_6 := 3 - (impulsive_6)] # plan trips well ahead of time
-quest[, impulsive_7 := 3 - (impulsive_7)] # self-controlled
-quest[, impulsive_8 := 3 - (impulsive_8)] # concentrate easily
-quest[, impulsive_9 := 3 - (impulsive_9)] # save regularly
-quest[, impulsive_11 := 3 - (impulsive_11)] # am a careful thinker
-quest[, impulsive_12 := 3 - (impulsive_12)] # plan for job security
-quest[, impulsive_14 := 3 - (impulsive_14)] # like to think
-quest[, impulsive_19 := 3 - (impulsive_19)] # am a steady thinker
-quest[, impulsive_28 := 3 - (impulsive_28)] # I like puzzles
-quest[, impulsive_29 := 3 - (impulsive_29)] # future oriented
 
-# Pleasure - higher is not feeling pleasure
-quest[, pleasure_0 := 3 - (pleasure_0)]
-quest[, pleasure_1 := 3 - (pleasure_1)]
-quest[, pleasure_2 := 3 - (pleasure_2)]
-quest[, pleasure_3 := 3 - (pleasure_3)]
-quest[, pleasure_4 := 3 - (pleasure_4)]
-quest[, pleasure_5 := 3 - (pleasure_5)]
-quest[, pleasure_6 := 3 - (pleasure_6)]
-quest[, pleasure_7 := 3 - (pleasure_7)]
-quest[, pleasure_8 := 3 - (pleasure_8)]
-quest[, pleasure_9 := 3 - (pleasure_9)]
-quest[, pleasure_10 := 3 - (pleasure_10)]
-quest[, pleasure_11 := 3 - (pleasure_11)]
-quest[, pleasure_12 := 3 - (pleasure_12)]
-quest[, pleasure_13 := 3 - (pleasure_13)]
+# Regulatory mode - higher is motivated.
+quest[, reg_Q13 := 5 - reg_Q13] # wait a little
+quest[, reg_Q24 := 5 - reg_Q24] # low energy
 
-# Regulatory focus - higher is assessment. Rename Lie items
-quest[, reg_focus_0 := 5 - reg_focus_0] # doing things even if they involve extra effort
-quest[, reg_focus_1 := 5 - reg_focus_1] # never evaluate my social interactions
-quest[, reg_focus_2 := 5 - reg_focus_2] # workaholic
-quest[, reg_focus_3 := 5 - reg_focus_3] # excited before goal
-quest[, reg_focus_4 := 5 - reg_focus_4] # enjoy actively doing things
-quest[, reg_focus_7 := 5 - reg_focus_7] # I am a doer
-quest[, reg_focus_9 := 5 - reg_focus_9] # don't spend much time thinking
-setnames(quest, 'reg_focus_11', 'lie_1') # one should never engage in leisure activities
-setnames(quest, 'reg_focus_13', 'lie_2') # never been late for work
-quest[, reg_focus_15 := 5 - reg_focus_15] # I can't wait to get started
-setnames(quest, 'reg_focus_16', 'lie_3') # always make the right decision
-setnames(quest, 'reg_focus_17', 'lie_4') # never find faults in someone I like
-quest[, reg_focus_20 := 5 - reg_focus_20] # already have the next one in mind
-setnames(quest, 'reg_focus_22', 'lie_5') # never hurt another person's feelings
-quest[, reg_focus_24 := 5 - reg_focus_24] # occupied with the task I wish to accomplish
-setnames(quest, 'reg_focus_25', 'lie_6')# no such thing as an honest mistake
-quest[, reg_focus_26 := 5 - reg_focus_26] # rarely analyze the conversations
-quest[, reg_focus_27 := 5 - reg_focus_27]# I usually persevere
-quest[, reg_focus_28 := 5 - reg_focus_28]# go-getter
 
 # Save questionnaire to file
 write.csv(renamePIDs(quest), file = file.path(preprocDatDir, "quest_data.csv"))
