8f787178846735066ae9dcf667f0994901de3981
yanivabir <yanivabir@gmail.com>
2023-05-02 15:08:49 +0300

    change PIDs from invitationId to userId

--- fix_first_sample_id.R
+++ fix_first_sample_id.R
@@ -5,22 +5,37 @@ library(data.table)
 sampleName <- "v1.0"
 preprocDatDir <- file.path("..", "data", sampleName, "preproc")
 rawDatDir <- file.path("..", "data", sampleName, "raw")
+midgamDatDir <- file.path("..", "data", sampleName, "midgam")
 
-invite <- fread(file.path(preprocDatDir, "invite.csv"))
-invite$V1 <- NULL
+midgam_list <- fread(file.path(midgamDatDir, "dshoamy2023042701.csv"))
 
-files <- list.files(rawDatDir, pattern = "second")
+files <- list.files(rawDatDir, pattern = "sess1")
 
-getPID <- function(s) substring(strsplit(s,"_")[[1]][1],2)
+renamePIDs <- function(dat) dat[.(PID = midgam_list$invitationId, 
+                                  to = midgam_list$userId), 
+                                on = "PID", 
+                                PID := i.to]
 
 for (f in files){
-  uid <- getPID(f)
-  userid <- invite[invitationId == uid]$userId
   
-  file.rename(file.path(rawDatDir, paste0("S",uid, "_secondSessStims.csv")),
-              file.path(rawDatDir, paste0("S", userid, "_secondSessStims.csv")))
+  # Get new PID
+  PID <- substring(strsplit(f,"_")[[1]][1],2)
+  print(paste0("working on ", PID))
+  PID <- midgam_list[invitationId == PID]$userId
+  
+  # Load data
+  dat <- fread(file.path(rawDatDir, f))
+  
+  # Rename PIDs
+  if (!is.null(dat$PID)){
+    dat <- renamePIDs(dat)
+  }
+  
+  # Get file name
+  fname <- paste0(strsplit(f,"_")[[1]][-1], collapse = "_")
+  fname <- paste0("S", PID, "_", fname)
+  
+  # Save data
+  print(paste0("Saving to ", fname))
+  write.csv(dat, file = file.path(preprocDatDir, fname))
 }
-
-inviteids <- unlist(lapply(files, getPID))
-
-invite[!(invitationId %in% inviteids)]
