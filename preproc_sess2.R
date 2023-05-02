906129b29f352f24d35efcb521876a9c43834295
yanivabir <yanivabir@gmail.com>
2023-05-02 21:17:14 +0300

    work on preproc sess 2

--- preproc_sess2.R
+++ preproc_sess2.R
@@ -13,3 +13,94 @@ files <- list.files(rawDatDir, pattern = "sess2")
 mfiles <- files[grepl(".csv", files, fixed =T) & !grepl("int", files)]
 intfiles <- files[grepl(".csv", files, fixed =T) & grepl("int", files)]
 
+# Open each file, and then rbind them together
+data <- do.call(rbind, lapply(mfiles, function(f) fread(file.path(rawDatDir, f))))
+
+# PID to string
+data[, PID := as.character(PID)]
+
+getPID <- function(s) substring(strsplit(s,"_")[[1]][1],2)
+
+int_data <- do.call(rbind, lapply(intfiles, function(f) {
+  dat <- fread(file.path(rawDatDir, f))
+  dat$PID <- getPID(f)
+  return(dat)}))
+
+# Remove kickouts ----
+kickouts <- data[, .(kickout = sum(category == "kick-out")), by = PID][kickout>0]
+write.csv(kickouts, file = file.path(preprocDatDir, "kickouts_sess2.csv"))
+data <- data[!(PID %in% kickouts$PID)]
+int_data <- int_data[!(PID %in% kickouts$PID)]
+
+# Quality measures ----
+# Add trial category to interaction data to see if any occurred in important bits
+int_data <- merge(int_data, 
+                  data[, .(PID, trial_index, category)],
+                  by.x = c("PID","trial"),
+                  by.y = c("PID", "trial_index"),
+                  all.x = TRUE)
+
+recall_cats <- c("answer_recall", "answer_recall_yn")
+
+int_data[, important := category %in% recall_cats]
+
+# Create session quality table
+quality <- int_data[, .(interactions = .N,
+                        important_interactions = sum(important)), by = PID]
+
+# Write interaction data to file
+write.csv(int_data, file.path(preprocDatDir, "browser_interactions_sess2.csv"))
+
+# Add interactions to main datatable
+int_sum <- int_data[, .(events = paste(event, collapse=",")), by = c("PID", "trial")]
+data <- merge(data, int_sum[, .(PID, trial, events)],
+              by.x = c("PID", "trial_index"),
+              by.y = c("PID", "trial"), 
+              all.x = TRUE)
+
+# Experiment duaration
+duration <- data[, .(duartion = (max(time_elapsed) - min(time_elapsed))/60000), 
+                 by = PID]
+quality <- merge(quality, duration, all.x = TRUE)
+
+# Number of warnings given
+warnings <- data[, .(warnings = max(n_warnings)), by=PID]
+quality <- merge(quality, warnings, all.x=TRUE)
+
+## Preprocess recall data ----
+# Parse text responses
+recall_text <- data[category == "answer_recall"]
+
+recall_text[, recall := fromJSON(gsub('""', '"', responses))$recall,
+       by = .(PID, trial_index)]
+
+# Gather forced choice responses
+recall_yn <- data[category == "answer_recall_yn"]
+recall_yn[button_pressed == "0", choice := "yes"]
+recall_yn[button_pressed == "1", choice := "no"]
+
+recall <- merge(recall_yn[, .(PID, sess, firstBlock, version, block, type, 
+                              trial_index, questionId, question, 
+                              answer, choice)], 
+                recall_text[, .(PID, questionId, recall)],
+                by = c("PID", "questionId"), all.x = T)[order(PID, trial_index)]
+assert("NAs in recall data", sum(is.na(recall[choice == "yes"]$recall)) == 0)
+
+# Remove html from stimuli
+recall[, question := gsub("<div class='question'>|</div>", "", question)]
+recall[, answer := gsub("<div class='answer'>|</div>", "", answer)]
+
+# Score easy scores ----
+
+# Mark numeric answers
+suppressWarnings(recall[!is.na(recall), is_number := !is.na(as.numeric(answer)), 
+       by = .(PID, trial_index)])
+
+# Calculate string distance
+recall[!is.na(recall), distance := adist(answer, recall, fixed = F, 
+                                         ignore.case = T),
+       by = .(PID, trial_index)]
+
+# If numeric and exact match, or non numeric and dist < 1, mark as true
+recall[(!is.na(recall)) & ((is_number & (distance == 0)) |
+                           (!is_number & (distance < 2))), correct := T]
