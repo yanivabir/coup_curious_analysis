92a34bdcbffb32a1d9c6145de94a94e313b73290
yanivabir <yanivabir@gmail.com>
2023-06-24 19:30:22 +0300

    remove devtools

--- docker/install_cmdstan.R
+++ docker/install_cmdstan.R
@@ -1,11 +1,13 @@
 # Install cmdstanr
-library(devtools)
 options(Ncpus = 12)
-install_version("cmdstanr", version="0.5.3", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+install.packages("remotes")
+remotes::install_version("cmdstanr", version="0.5.3", 
+                         repos = c("https://mc-stan.org/r-packages/", 
+                                   getOption("repos")))
 library(cmdstanr)
 
 dir.create(file.path("/home/rstudio/", ".cmdstanr"), recursive = TRUE)
-install_cmdstan(version = "2.32.0", 
+install_cmdstan(version = "2.32.2", 
     cores = 11, 
     dir = file.path("/home/rstudio/", ".cmdstanr"),
     cpp_options = list("CXX" = "clang++"))
